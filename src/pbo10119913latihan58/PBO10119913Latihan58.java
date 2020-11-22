93529b9b41b1143c242b17289b1413f641b95313
Dilan <ramadhanrizqi21@gmail.com>
2020-11-22 08:45:03 +0700

    Latihan 58

---
+++ src/pbo10119913latihan58/PBO10119913Latihan58.java
@@ -0,0 +1,26 @@
+ /*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo10119913latihan58;
+
+/**
+ * Nama :Muhammad Alvin Rizqi Ramdahn
+ * Kelas : IF10K
+ * NIM : 10119913
+ * @author Lenovo
+ */
+public class PBO10119913Latihan58 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        JumlahBilangan bilangan1 = new JumlahBilangan();
+        bilangan1.tampilHasilJumlah();
+        SelisihBilangan oo2 = new SelisihBilangan();
+        oo2.tampilSelisih();
+    }
+    
+}
